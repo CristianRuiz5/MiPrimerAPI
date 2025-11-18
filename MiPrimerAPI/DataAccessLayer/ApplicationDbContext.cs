5fe05a8a6c02c89ce5b65ca92e03d329211d66e6
CRISTIAN RUIZ L�PEZ <cristianruiz309594@correo.itm.edu.co>
2025-11-18 18:29:53 -0500

    com0.0Config

---
+++ MiPrimerAPI/DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiPrimerAPI.DataAccessLayer.Models;
+
+namespace MiPrimerAPI.DataAccessLayer
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+
+        //Seccion para crear el dbSET de las entidaedes o modelos
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ MiPrimerAPI/DataAccessLayer/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiPrimerAPI.DataAccessLayer.Models
+{
+    public class Category : AuditBase
+
+    {
+        [Required] //Este data annotatiopn indica que el campo es obligatorio
+        [Display(Name="Nombre de la categoria")]//Me sirve para personalizar el nombre que se muestra en las vistas o mensajes de error
+        public string Name { get; set; }
+
+    }
+}
+
+////*
+// * Categories
+// * --Id
+// * --Name
+// * --CreatedDate
+// * --ModifiedDate
